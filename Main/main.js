9e34845462e27a0ada59d2fc864e0dacf1a5f8de
DuanWebthuctap <21T1020678@husc.edu.vn>
2024-08-17 23:52:30 +0700

    ok

---
+++ Main/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var bannerEnd = document.querySelector('.banner-end');
+    if (window.scrollY > 100) { // Adjust this value to control when the div appears
+        bannerEnd.classList.add('visible');
+    } else {
+        bannerEnd.classList.remove('visible');
+    }
+});
